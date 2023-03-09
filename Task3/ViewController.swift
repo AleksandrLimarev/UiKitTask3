b732b2085ab4ff60f2c64e4f4c90c4d6b9c2ff29
Aleksandr Limarev <limarev.sanya@gmail.com>
2023-03-09 11:40:34 +0100

    UiKit

--- Task3/ViewController.swift
+++ Task3/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController {
     let animationDuration: TimeInterval = 0.3
     let maxScale: CGFloat = 1.5
     let maxRotation: CGFloat = .pi / 2
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        view.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         setupSquareView()
         setupSlider()
     }
-
+    
     func setupSquareView() {
         let topInset = view.safeAreaInsets.top + 100.0
         squareView = UIView(frame: CGRect(x: view.layoutMargins.left, y: topInset, width: squareSize, height: squareSize))
@@ -29,8 +29,8 @@ class ViewController: UIViewController {
         squareView.layer.cornerRadius = 10.0
         view.addSubview(squareView)
     }
-
-
+    
+    
     func setupSlider() {
         slider = UISlider()
         slider.translatesAutoresizingMaskIntoConstraints = false
@@ -43,19 +43,19 @@ class ViewController: UIViewController {
             slider.topAnchor.constraint(equalTo: squareView.bottomAnchor, constant: 40)
         ])
     }
-
-
+    
+    
     @objc func sliderChanged(_ sender: UISlider) {
-            let x = sender.value * Float(view.bounds.width - view.layoutMargins.left - view.layoutMargins.right - squareSize)
-            let scale = 1 + CGFloat(sender.value) * (maxScale - 1)
-            let rotation = CGFloat(sender.value) * maxRotation
-            UIView.animate(withDuration: animationDuration) {
-                self.squareView.transform = CGAffineTransform(translationX: CGFloat(x), y: 0)
-                    .scaledBy(x: scale, y: scale)
-                    .rotated(by: rotation)
-            }
+        let x = sender.value * Float(view.bounds.width - view.layoutMargins.left - view.layoutMargins.right - squareSize) - 20
+        let scale = 1 + CGFloat(sender.value) * (maxScale - 1)
+        let rotation = CGFloat(sender.value) * maxRotation
+        UIView.animate(withDuration: animationDuration) {
+            self.squareView.transform = CGAffineTransform(translationX: CGFloat(x), y: 0)
+                .scaledBy(x: scale, y: scale)
+                .rotated(by: rotation)
         }
-
+    }
+    
     @objc func sliderReleased(_ sender: UISlider) {
         sender.setValue(1, animated: true)
         sliderChanged(sender)
